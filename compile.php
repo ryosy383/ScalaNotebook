8399f922dcb7697265759779533f9ffa85cb4a18
ryosy383 <mosso.ryosy383@gmail.com>
2014-07-02 12:49:11 +0900

    以前のコンパイルの出力が残るバグ修正

--- compile.php
+++ compile.php
@@ -3,8 +3,10 @@
 	$fp = fopen('code.scala', 'w');
 	fwrite($fp, $code);
 	fclose($fp);
-	shell_exec('scalac -classpath . code.scala');
-      shell_exec('code > out.txt');
+	shell_exec('echo -n > out.txt');
+	shell_exec('rm *.class');
+	shell_exec('scalac code.scala');
+	shell_exec('scala DisplayFeed > out.txt');
 	
 	$result =  file_get_contents('out.txt');
 	
